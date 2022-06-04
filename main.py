b292ac195e48521a78c57bfb27ce85ee59776312
Sheikh Fahim Faysal Sowrav <gfahim38@gmail.com>
2022-06-04 17:51:01 +0600

    Module 09 V.1.0

---
+++ main.py
@@ -0,0 +1,53 @@
+#
+'''nums = [55, 44, 33, 22, 11]
+
+if all([i > 5 for i in nums]):
+    print("All larger than 5")
+
+if any([i % 2 == 0 for i in nums]):
+    print("At least one is even")
+
+for v in enumerate(nums):
+    print(v)
+
+str="{c}, {b}, {a}".format(a=5, b=9, c=7)
+print(str)
+
+evens=[i**2 for i in range(10) if i**2 % 2 == 0]
+print(evens)
+
+nums = [i*2 for i in range(10)]
+print (nums)
+
+sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(sqs[7:5:-1])
+
+tuple = (1, (1, 2, 3))
+print(tuple[1])
+
+pairs = {1: "apple",
+        "orange": [2, 3, 4],
+         True: False,
+        None: "True",
+    }
+print(pairs.get("orange"))
+print(pairs.get(7))
+print(pairs.get(12345, "not in dictionary"))
+
+fib = {1: 1, 2: 1, 3: 2, 4: 3}
+print(fib.get(4, 0) + fib.get(7, 5))
+
+foo = print()
+if foo == None:
+   print(1)
+else:
+   print(2)'''
+
+
+
+
+
+
+
+
+
